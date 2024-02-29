eca9bf8883082fbc165ecc05d2b40ab8e903e009
Jackson VFB <jackvfb@gmail.com>
2024-02-29 14:47:28 -0800

    Update train.gps.R
    Had to remove a GPS point that was over land.

--- data-raw/train.gps.R
+++ data-raw/train.gps.R
@@ -6,6 +6,8 @@ library(tidyverse)
 gpsFiles <- list.files("C:/Users/jackv/Documents/thesis-data/gps", pattern = ".csv", full.names = TRUE)
 gpsAll <- purrr::map(gpsFiles, read_csv, col_select = c("Latitude", "Longitude", "UTC", "DriftName", "seadepth"))
 names(gpsAll) <- basename(gpsFiles)
+#remove point over land for one of the drifts in the training set.
+gpsAll[["OPPS_010_GPS.csv"]] <- gpsAll[["OPPS_010_GPS.csv"]][1,]
 gpsAll <- list_rbind(gpsAll)
 
 # Helper function
